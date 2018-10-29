6415fd1f6c79587f6318508718a2fd20e5168987
EXPERIS <experisit@MacBook-Pro-de-EXPERIS.local>
2018-10-29 15:55:49 +0100

    delete cypress example folder and update server js

--- server/index.js
+++ server/index.js
@@ -4,15 +4,15 @@ const data = require("./phones.json");
 
 
 app.get('/',(req,res)=>{
- res.send("Please use htto://localhost 4000/api/phones to fetch all the phones' data");
+ res.send("Please use http://localhost 4000/api/phones to fetch all the phones' data");
 })
 
 app.get('/api/phones',(req,res)=>{
     res.send(data);
 });
 
-app.get('/api/iphone',(req,res)=>{
-    const iphone=data.filter((phone)=>phone.brand='iphone');
+app.get('/api/apple',(req,res)=>{
+    const iphone=data.filter((phone)=>phone.brand='apple');
     res.send(iphone);
 });
 
@@ -20,10 +20,18 @@ app.get('/api/samsung',(req,res)=>{
     const samsumg=data.filter((phone)=>phone.brand==='samsung');
     res.send(samsumg);
 }); 
+app.get('/api/huawei',(req,res)=>{
+    const samsumg=data.filter((phone)=>phone.brand==='huawei');
+    res.send(samsumg);
+});
+app.get('/api/xiaomi',(req,res)=>{
+    const samsumg=data.filter((phone)=>phone.brand==='xiaomi');
+    res.send(samsumg);
+});
 
-app.get('/api/iphone/:model',(req,res)=>{
+app.get('/api/apple:model',(req,res)=>{
     let phone=data.filter((phone)=>
-    phone.model.toLowerCase()===req.params.model.toLowerCase()&& phone.brand==='iphone'
+    phone.model.toLowerCase()===req.params.model.toLowerCase()&& phone.brand==='apple'
     )
     phone.length !==0 ? res.send(phone):res.status(404).send('Can´t find this phone !!')
 });
@@ -33,6 +41,27 @@ app.get('/api/samsung/:model',(req,res)=>{
     phone.model.toLowerCase()===req.params.model.toLowerCase()&& phone.brand==='samsung'
     )
    phone.length !==0 ? res.send(phone):res.status(404).send('Can´t find this phone !!')
+   
+});
+app.get('/api/huawei/:model',(req,res)=>{
+    let phone=data.filter((phone)=>
+    phone.model.toLowerCase()===req.params.model.toLowerCase()&& phone.brand==='huawei'
+    )
+   phone.length !==0 ? res.send(phone):res.status(404).send('Can´t find this phone !!')
+   
+});
+app.get('/api/xiaomi/:model',(req,res)=>{
+    let phone=data.filter((phone)=>
+    phone.model.toLowerCase()===req.params.model.toLowerCase()&& phone.brand==='xiaomi'
+    )
+   phone.length !==0 ? res.send(phone):res.status(404).send('Can´t find this phone !!')
+   
+});
+app.get('/api/:product',(req,res)=>{
+    let phone=data.filter((phone)=>
+    phone.product.toLowerCase()===req.params.product.toLowerCase()
+    )
+   phone.length !==0 ? res.send(phone):res.status(404).send('Can´t find this product !!')
    
 });
 
