13149f87d8a43ffdc713fbd33d093dc128bd04c0
EXPERIS <experisit@MacBook-Pro-de-EXPERIS.local>
2018-10-29 16:34:17 +0100

    add rel="noopener noreferrer" to link with target='_blamc'

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,10 +4,10 @@ class Footer extends Component{
     render(){
         return(
             <div className="footer">
-            <p>A project by <a href="https://github.com/lucas1004jx" target="_blank">Xin jin</a></p>
+            <p>A project by <a href="https://github.com/lucas1004jx" target="_blank" rel="noopener noreferrer">Xin jin</a></p>
             </div>
         )
     }
-}
+} 
 
 export default Footer;
